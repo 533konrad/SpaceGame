6fdc86c71243ffdb183cf2c8092c89985132044f
Konrad Gładkowski <konrad@greenparrot.pl>
2022-09-12 22:27:43 +0200

    V3

--- main.py
+++ main.py
@@ -1,181 +1,199 @@
-import pygame
+import pygame, sys
 import os
 pygame.font.init()
 pygame.mixer.init()
 
-WIDTH, HEIGHT = 900, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("First Game!")
+#ustawienie ikony naszej gry
+ikona = pygame.image.load(os.path.join('Assets','icon.jpeg'))
+pygame.display.set_icon(ikona)
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-YELLOW = (255, 255, 0)
-
-BORDER = pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
-
-#BULLET_HIT_SOUND = pygame.mixer.Sound('Assets/Grenade+1.mp3')
-#BULLET_FIRE_SOUND = pygame.mixer.Sound('Assets/Gun+Silencer.mp3')
-
-HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
-WINNER_FONT = pygame.font.SysFont('comicsans', 100)
-
+#stałe parametry
+SZEROKOSC, DLUGOSC = 900, 600
+SZEROKOSC_MARGINESU = 20
+SZEROKOSC_OBIEKTU, DLUGOSC_OBIEKTU = 40, 40
+PREDKOSC = 5
+PREDKOSC_POCISKU = 7
+MAX_POCISKOW = 3
 FPS = 60
-VEL = 5
-BULLET_VEL = 7
-MAX_BULLETS = 3
-SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
-YELLOW_HIT = pygame.USEREVENT + 1
-RED_HIT = pygame.USEREVENT + 2
+#event trafienia gracza
+GRACZ1_TRAFIONY = pygame.USEREVENT + 1
+GRACZ2_TRAFIONY = pygame.USEREVENT + 2
 
-YELLOW_SPACESHIP_IMAGE = pygame.image.load(
-    os.path.join('Assets', 'spaceship_yellow.png'))
-YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
-    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+#obrazek na tło statku
+ZOLTY_STATEK_OBRAZEK = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+CZERWONY_STATEK_OBRAZEK = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
 
-RED_SPACESHIP_IMAGE = pygame.image.load(
-    os.path.join('Assets', 'spaceship_red.png'))
-RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
-    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+#pygame.transform.rotate("co obracamy", "o jaki stopień obracamy")
+ZOLTY_STATEK = pygame.transform.rotate(pygame.transform.scale(ZOLTY_STATEK_OBRAZEK, (SZEROKOSC_OBIEKTU, DLUGOSC_OBIEKTU)), 90)
+CZERWONY_STATEK = pygame.transform.rotate(pygame.transform.scale(CZERWONY_STATEK_OBRAZEK, (SZEROKOSC_OBIEKTU, DLUGOSC_OBIEKTU)), 270)
 
-SPACE = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'space.png')), (WIDTH, HEIGHT))
+#obrazek na tło planszy
+GWIAZDY = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'space.png')), (SZEROKOSC,DLUGOSC))
 
+#dodajemy font do wyświetlania ilości żyć obu graczy
+ZYCIA_FONT = pygame.font.SysFont("helvetica", 25)
+AUTOR_FONT = pygame.font.SysFont("helvetica", 20)
 
-def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health):
-    WIN.blit(SPACE, (0, 0))
-    pygame.draw.rect(WIN, BLACK, BORDER)
+#font do wyświetlania informacji o zwycięzcy. Helvetica - 50
+ZWYCIEZCA_FONT = pygame.font.SysFont("helvetica", 90)
 
-    red_health_text = HEALTH_FONT.render(
-        "Health: " + str(red_health), True, WHITE)
-    yellow_health_text = HEALTH_FONT.render(
-        "Health: " + str(yellow_health), True, WHITE)
-    WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
-    WIN.blit(yellow_health_text, (10, 10))
+#zaimportować dźwięk do naszej gry
+ODGLOS_STRZAL = pygame.mixer.Sound(os.path.join('Assets','Gun+Silencer.mp3'))
+ODGLOS_TRAFIENIE = pygame.mixer.Sound(os.path.join('Assets', 'Grenade+1.mp3'))
+ODGLOS_FANFARY = pygame.mixer.Sound(os.path.join('Assets','Ta-Dah.mp3'))
 
-    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
-    WIN.blit(RED_SPACESHIP, (red.x, red.y))
+#załadowanie i odtwarzanie muzyki w tle
+pygame.mixer.music.load(os.path.join('Assets','Pipe.mp3'))
+pygame.mixer.music.play(-1,0.0)
 
-    for bullet in red_bullets:
-        pygame.draw.rect(WIN, RED, bullet)
+#tworzymy nową klasę z naszą grą
+class MojaGra:
+    #tworzymy konstruktor
+    def __init__(self):
+        # inicjujemy pygame
+        pygame.init()
+        # tworzymy nasze okno gry
+        self.okno = pygame.display.set_mode((SZEROKOSC, DLUGOSC))
+        pygame.display.set_caption("Pierwsza gra Konrada!")
 
-    for bullet in yellow_bullets:
-        pygame.draw.rect(WIN, YELLOW, bullet)
+        self.gracz1 = pygame.Rect(100, 100, SZEROKOSC_OBIEKTU, DLUGOSC_OBIEKTU)
+        self.gracz2 = pygame.Rect(700, 100, SZEROKOSC_OBIEKTU, DLUGOSC_OBIEKTU)
 
-    pygame.display.update()
+        self.pociski_gracz1 = []
+        self.pociski_gracz2 = []
 
+        self.granica = pygame.Rect(SZEROKOSC//2 - 5, 0, 10, DLUGOSC)
 
-def yellow_handle_movement(keys_pressed, yellow):
-    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:  # LEFT
-        yellow.x -= VEL
-    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x:  # RIGHT
-        yellow.x += VEL
-    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:  # UP
-        yellow.y -= VEL
-    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 15:  # DOWN
-        yellow.y += VEL
+        self.zycia_gracz1 = self.zycia_gracz2 = 10
 
+    def start(self):
+        clock = pygame.time.Clock()
+        while True:
+            clock.tick(FPS)
+            self.obslugaEventow()
+            self.obslugaKlawiszy()
+            self.obslugaPociskow()
+            self.obslugaPunktow()
+            self.obslugaEkranu()
 
-def red_handle_movement(keys_pressed, red):
-    if keys_pressed[pygame.K_LEFT] and red.x - VEL > BORDER.x + BORDER.width:  # LEFT
-        red.x -= VEL
-    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width < WIDTH:  # RIGHT
-        red.x += VEL
-    if keys_pressed[pygame.K_UP] and red.y - VEL > 0:  # UP
-        red.y -= VEL
-    if keys_pressed[pygame.K_DOWN] and red.y + VEL + red.height < HEIGHT - 15:  # DOWN
-        red.y += VEL
+    def obslugaPociskow(self):
+        for pocisk in self.pociski_gracz1:
+            pocisk.x += PREDKOSC_POCISKU
+            # jeżeli pocisk uderzy w przeciwnika to znika z talbicy pociski_gracza
+            if self.gracz2.colliderect(pocisk):
+                #informacja, że gracz został trafiony
+                pygame.event.post(pygame.event.Event(GRACZ2_TRAFIONY))
+                #pocisk znika z tablicy pociski-gracza
+                self.pociski_gracz1.remove(pocisk)
+            # jeżeli pocisk wyleci poza ekran to znika z tablicy pociski_gracza
+            elif pocisk.x > SZEROKOSC:
+                self.pociski_gracz1.remove(pocisk)
 
+        for pocisk in self.pociski_gracz2:
+            pocisk.x -= PREDKOSC_POCISKU
+            if self.gracz1.colliderect(pocisk):
+                pygame.event.post(pygame.event.Event(GRACZ1_TRAFIONY))
+                self.pociski_gracz2.remove(pocisk)
+            elif pocisk.x < -10:
+                self.pociski_gracz2.remove(pocisk)
 
-def handle_bullets(yellow_bullets, red_bullets, yellow, red):
-    for bullet in yellow_bullets:
-        bullet.x += BULLET_VEL
-        if red.colliderect(bullet):
-            pygame.event.post(pygame.event.Event(RED_HIT))
-            yellow_bullets.remove(bullet)
-        elif bullet.x > WIDTH:
-            yellow_bullets.remove(bullet)
+    def obslugaPunktow(self):
+        tekst_zwyciezca = ""
+        #sprawdzamy, czy któryś z graczy ma mniej niż 1 punkt życia
+        if self.zycia_gracz1 <= 0:
+            tekst_zwyciezca = "Gracz 2 wygrał!"
+        if self.zycia_gracz2 <= 0:
+            tekst_zwyciezca = "Gracz 1 wygrał!"
 
-    for bullet in red_bullets:
-        bullet.x -= BULLET_VEL
-        if yellow.colliderect(bullet):
-            pygame.event.post(pygame.event.Event(YELLOW_HIT))
-            red_bullets.remove(bullet)
-        elif bullet.x < 0:
-            red_bullets.remove(bullet)
+        if tekst_zwyciezca:
+            #duży napis na środku z tekstem tekst_zwyciezca
+            #print(tekst_zwyciezca)
+            zwyciezca = ZWYCIEZCA_FONT.render(tekst_zwyciezca, True, (200,200,200))
+            self.okno.blit(zwyciezca, (SZEROKOSC/2 - zwyciezca.get_width()/2, DLUGOSC/2 - zwyciezca.get_height()/2))
+            ODGLOS_FANFARY.play()
+            pygame.display.update()
+            pygame.time.delay(5000)
+            self.__init__()
 
+    def obslugaEkranu(self):
+        self.okno.blit(GWIAZDY, (0,0))
+        #rysujemy ile punktów życia ma gracz 1
+        gracz1_zycie = ZYCIA_FONT.render("Życie: " + str(self.zycia_gracz1), True, (230,230,230))
+        self.okno.blit(gracz1_zycie, (10,10))
 
-def draw_winner(text):
-    draw_text = WINNER_FONT.render(text, True, WHITE)
-    WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() /
-                         2, HEIGHT/2 - draw_text.get_height()/2))
-    pygame.display.update()
-    pygame.time.delay(5000)
+        #rysujemy ile punktów życia ma gracz 2
+        gracz2_zycie = ZYCIA_FONT.render("Życie:" + str(self.zycia_gracz2), True, (230,230,230))
+        self.okno.blit(gracz2_zycie, (790,10))
 
+        #podpis autora "Autor gry: IMIE, 2021 rok"
+        #stworzyć nowy font: AUTOR_FONT: Helvetica. wielkość fontu: 12. umiejscowienie: prawy dolny róg ekranu. kolor: jasnoniebieski
+        #ZYCIA_FONT = pygame.font.SysFont("helvetica", 25)
+        copyright = AUTOR_FONT.render("Konrad, 2021 r.", True, (160, 200, 180))
+        self.okno.blit(copyright, (780, 570))
 
-def main():
-    red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
-    yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+        pygame.draw.rect(self.okno, (5, 5, 5), self.granica)
+        self.okno.blit(ZOLTY_STATEK, (self.gracz1.x,self.gracz1.y))
+        self.okno.blit(CZERWONY_STATEK, (self.gracz2.x,self.gracz2.y))
 
-    red_bullets = []
-    yellow_bullets = []
+        for pocisk in self.pociski_gracz1:
+            pygame.draw.rect(self.okno, (255,0,0), pocisk)
+        for pocisk in self.pociski_gracz2:
+            pygame.draw.rect(self.okno, (255,0,0), pocisk)
 
-    red_health = 10
-    yellow_health = 10
+        pygame.display.flip()
 
-    clock = pygame.time.Clock()
-    run = True
-    while run:
-        clock.tick(FPS)
+    def obslugaKlawiszy(self):
+        # wszystkie wcisniete klawisze
+        klawisze = pygame.key.get_pressed()
+        # poruszanie kwadratu lewo/prawo graczem 1
+        if klawisze[pygame.K_RIGHT] and self.gracz1.x < self.granica.x - SZEROKOSC_OBIEKTU:
+            self.gracz1.x += PREDKOSC
+        elif klawisze[pygame.K_LEFT] and self.gracz1.x > SZEROKOSC_MARGINESU:
+            self.gracz1.x -= PREDKOSC
+        elif klawisze[pygame.K_UP] and self.gracz1.y > SZEROKOSC_MARGINESU:
+            self.gracz1.y -= PREDKOSC
+        elif klawisze[pygame.K_DOWN] and self.gracz1.y < DLUGOSC - SZEROKOSC_OBIEKTU - SZEROKOSC_MARGINESU:
+            self.gracz1.y += PREDKOSC
+
+        # poruszanie kwadratu lewo/prawo graczem 2
+        if klawisze[pygame.K_d] and self.gracz2.x < SZEROKOSC - SZEROKOSC_OBIEKTU - SZEROKOSC_MARGINESU:
+            self.gracz2.x += PREDKOSC
+        elif klawisze[pygame.K_a] and self.gracz2.x > self.granica.x + self.granica.width:
+            self.gracz2.x -= PREDKOSC
+        elif klawisze[pygame.K_w] and self.gracz2.y > SZEROKOSC_MARGINESU:
+            self.gracz2.y -= PREDKOSC
+        elif klawisze[pygame.K_s] and self.gracz2.y < DLUGOSC - SZEROKOSC_OBIEKTU - SZEROKOSC_MARGINESU:
+            self.gracz2.y += PREDKOSC
+
+    def obslugaEventow(self):
+        # nasłuchiwania akcji użytkowników
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
+                sys.exit(0)
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                sys.exit(0)
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
-                    bullet = pygame.Rect(
-                        yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
-                    yellow_bullets.append(bullet)
-                    #BULLET_FIRE_SOUND.play()
+            #obsługa strzelania pierwszego gracza jako event
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_m and len(self.pociski_gracz1) < MAX_POCISKOW:
+                pocisk = pygame.Rect(self.gracz1.x + self.gracz1.width, self.gracz1.y + self.gracz1.height // 2, 10, 5)
+                self.pociski_gracz1.append(pocisk)
+                ODGLOS_STRZAL.play()
 
-                if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
-                    bullet = pygame.Rect(
-                        red.x, red.y + red.height//2 - 2, 10, 5)
-                    red_bullets.append(bullet)
-                    #BULLET_FIRE_SOUND.play()
+            #obsluga strzelania drugiego gracza jako event
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_c and len(self.pociski_gracz2) < MAX_POCISKOW:
+                pocisk = pygame.Rect(self.gracz2.x, self.gracz2.y + self.gracz2.height // 2, 10, 5)
+                self.pociski_gracz2.append(pocisk)
+                ODGLOS_STRZAL.play()
 
-            if event.type == RED_HIT:
-                red_health -= 1
-                #BULLET_HIT_SOUND.play()
+            #sprawdzenie czy event trafienia został wywołany
+            if event.type == GRACZ1_TRAFIONY:
+                self.zycia_gracz1 -= 1
+                ODGLOS_TRAFIENIE.play()
 
-            if event.type == YELLOW_HIT:
-                yellow_health -= 1
-                #BULLET_HIT_SOUND.play()
+            if event.type == GRACZ2_TRAFIONY:
+                self.zycia_gracz2 -= 1
+                ODGLOS_TRAFIENIE.play()
 
-        winner_text = ""
-        if red_health <= 0:
-            winner_text = "Yellow Wins!"
-
-        if yellow_health <= 0:
-            winner_text = "Red Wins!"
-
-        if winner_text != "":
-            draw_winner(winner_text)
-            break
-
-        keys_pressed = pygame.key.get_pressed()
-        yellow_handle_movement(keys_pressed, yellow)
-        red_handle_movement(keys_pressed, red)
-
-        handle_bullets(yellow_bullets, red_bullets, yellow, red)
-
-        draw_window(red, yellow, red_bullets, yellow_bullets,
-                    red_health, yellow_health)
-
-    main()
-
-
-if __name__ == "__main__":
-    main()
+gierka = MojaGra()
+gierka.start()
